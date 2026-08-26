3354ff53460f942c1493041331047330f0a46251
manutelw <manutelw@gmail.com>
2026-08-26 18:52:37 +0530

    Add ASCENT Play service worker

---
+++ ascent-play/sw.js
@@ -0,0 +1,5 @@
+const CACHE='ascent-play-v1';
+const CORE=['./index.html','./register.html','./home.html','./manifest.webmanifest'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).catch(()=>{}));self.skipWaiting()});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))});
