5146e639d2bdad328ca7e9fd838a0d4c5bb253a5
manutelw <manutelw@gmail.com>
2026-09-10 11:00:55 +0530

    Inject coherent vocabulary conversation layer

--- worker-oracy.js
+++ worker-oracy.js
@@ -94,6 +94,7 @@ async function applyTelwLevelBranding(response,unitNo){
   if(!html.includes('src="./level-system.js"')) html=html.replace('</body>','<script src="./level-system.js"></script>\n</body>');
   if(unitNo>=2 && !html.includes('unit-speaking-gym-v2.js')) html=html.replace('</body>','<script src="./unit-speaking-gym-v2.js?v=20260910"></script>\n</body>');
   if(unitNo>=2 && !html.includes('unit-speaking-model-natural.js')) html=html.replace('</body>','<script src="./unit-speaking-model-natural.js?v=20260910b"></script>\n</body>');
+  if(unitNo>=2 && !html.includes('unit-speaking-model-coherent.js')) html=html.replace('</body>','<script src="./unit-speaking-model-coherent.js?v=20260910"></script>\n</body>');
   const headers=new Headers(response.headers);
   headers.set('content-type','text/html; charset=UTF-8');
   headers.set('cache-control','no-store, max-age=0, must-revalidate');
