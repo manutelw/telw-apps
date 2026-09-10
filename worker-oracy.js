62b31b9f1de7c5127e7244419a373817970d8fae
manutelw <manutelw@gmail.com>
2026-09-10 11:16:12 +0530

    Load target-cued ORACY model layer

--- worker-oracy.js
+++ worker-oracy.js
@@ -95,6 +95,7 @@ async function applyTelwLevelBranding(response,unitNo){
   if(unitNo>=2 && !html.includes('unit-speaking-gym-v2.js')) html=html.replace('</body>','<script src="./unit-speaking-gym-v2.js?v=20260910"></script>\n</body>');
   if(unitNo>=2 && !html.includes('unit-speaking-model-natural.js')) html=html.replace('</body>','<script src="./unit-speaking-model-natural.js?v=20260910b"></script>\n</body>');
   if(unitNo>=2 && !html.includes('unit-speaking-model-coherent.js')) html=html.replace('</body>','<script src="./unit-speaking-model-coherent.js?v=20260910"></script>\n</body>');
+  if(unitNo>=2 && !html.includes('unit-speaking-model-target-cued.js')) html=html.replace('</body>','<script src="./unit-speaking-model-target-cued.js?v=20260910"></script>\n</body>');
   const headers=new Headers(response.headers);
   headers.set('content-type','text/html; charset=UTF-8');
   headers.set('cache-control','no-store, max-age=0, must-revalidate');
